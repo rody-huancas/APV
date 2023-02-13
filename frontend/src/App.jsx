72e688b0c7faf53c79c92a550502aa179d065852
Rody Huancas <rodyhuancas.04@gmail.com>
2023-02-12 20:11:22 -0500

    Paciente finalizado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,26 +9,32 @@ import NuevoPassword from "./pages/NuevoPassword";
 import AdministrarPacientes from "./pages/AdministrarPacientes";
 
 import { AuthProvider } from "./context/AuthProvider";
+import { PacientesProvider } from "./context/PacientesProvider";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          {/* área pública */}
-          <Route path="/" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-            <Route path="registrar" element={<Registrar />} />
-            <Route path="olvide-password" element={<OlvidePassword />} />
-            <Route path="olvide-password/:token" element={<NuevoPassword />} />
-            <Route path="confirmar/:id" element={<ConfimarCuenta />} />
-          </Route>
+        <PacientesProvider>
+          <Routes>
+            {/* área pública */}
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route
+                path="olvide-password/:token"
+                element={<NuevoPassword />}
+              />
+              <Route path="confirmar/:id" element={<ConfimarCuenta />} />
+            </Route>
 
-          {/* área privada */}
-          <Route path="/admin" element={<RutaProtegida />}>
-            <Route index element={<AdministrarPacientes />} />
-          </Route>
-        </Routes>
+            {/* área privada */}
+            <Route path="/admin" element={<RutaProtegida />}>
+              <Route index element={<AdministrarPacientes />} />
+            </Route>
+          </Routes>
+        </PacientesProvider>
       </AuthProvider>
     </BrowserRouter>
   );
--- frontend/src/components/Formulario.jsx
+++ frontend/src/components/Formulario.jsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Alerta from "./Alerta";
+import usePacientes from "../hooks/usePacientes";
 
 const Formulario = () => {
   const [nombre, setNombre] = useState("");
   const [propietario, setPropietario] = useState("");
   const [email, setEmail] = useState("");
-  const [fecha, setFecha] = useState(Date.now());
+  const [fecha, setFecha] = useState("");
   const [sintomas, setSintomas] = useState("");
+  const [id, setId] = useState(null);
 
   const [alerta, setAlerta] = useState({});
+
+  const { guardarPaciente, paciente } = usePacientes();
+
+  useEffect(() => {
+    if (paciente?.nombre) {
+      setNombre(paciente.nombre);
+      setPropietario(paciente.propietario);
+      setEmail(paciente.email);
+      setFecha(paciente.fecha);
+      setSintomas(paciente.sintomas);
+      setId(paciente._id);
+    }
+  }, [paciente]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,17 +36,30 @@ const Formulario = () => {
       });
       return;
     }
+
+    guardarPaciente({ nombre, propietario, email, fecha, sintomas, id });
+    setAlerta({
+      msg: "Guardado Correctamente",
+    });
+    setNombre("");
+    setPropietario("");
+    setEmail("");
+    setFecha("");
+    setSintomas("");
+    setId("");
   };
 
   const { msg } = alerta;
 
   return (
     <>
-      <p className="text-lg text-center mb-10">
+      <h2 className="font-black text-3xl text-center">
+        Administrador de Pacientes
+      </h2>
+      <p className="text-xl mt-5 mb-10 text-center">
         Añade tus Pacientes y {""}
         <span className="text-indigo-600 font-bold">Administralos</span>
       </p>
-
       <form
         className="bg-white py-10 px-5 mb-10 lg:mb-5 shadow-md rounded-md"
         onSubmit={handleSubmit}
@@ -108,8 +136,8 @@ const Formulario = () => {
 
         <input
           type="submit"
-          value="Agregar Paciente"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 hover:cursor-pointer transition-colors rounded-md"
+          value={id ? "Guardar Cambios" : "Agregar Paciente"}
         />
       </form>
 
